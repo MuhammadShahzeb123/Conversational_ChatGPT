de56bfed95109e6c0fea305b4825b0069645cac6
MuhammarShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-20 01:54:39 +0500

    Updated Functions

--- Functions.py
+++ Functions.py
@@ -1,17 +1,22 @@
 import openai
 
+openai.api_key = "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"
 
-openai.api_key = "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"
+class ChatGPT:
+    def __init__(self, role) -> None:
+        self.Messages =  [
+                {"role": "system", "content": role}       
+            ]
+    
+    def ask(self, Q):
+        self.Messages.append({"role": "user", "content": Q})
+        
+        responce = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=self.Messages
+        )
 
-def ask():
-    responce = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}
-        ]
-    )
+        
+        self.Messages.append({"role": "assistant", "content": responce["choices"][0]["message"]["content"]})
 
-    return responce["choices"][0]["message"]["content"]
+        return responce["choices"][0]["message"]["content"]
