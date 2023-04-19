1a4bf2a95026e8ed29e72312a391e6e4c408e06d
MuhammarShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-20 01:54:09 +0500

    Now it can remember the Previous Chat

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import Functions as F
+from time import sleep
 
-something = F.ask()
-print(something)
+role = "You are very angry and rude"
+something = F.ChatGPT(role=role)
+while True:
+    try:
+        Question = input("You: ")
+        if Question == "quit":
+            break
+        Answer = something.ask(Question)
+        Answer = "ChatGPT: " + Answer
+        for char in Answer:
+            print(char, end="", flush=True)
+            sleep(0.05)
+        print("\n")
+
+    except KeyboardInterrupt:
+        print("\n\nEnter 'quit' to exit")
