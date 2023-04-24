8f7635d708b50127f5d2d8f62afdab61cbd07bab
MuhammadShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-25 02:08:41 +0500

    Added Updating Features

--- Functions.py
+++ Functions.py
@@ -2,14 +2,13 @@ import openai
 import sys
 import os
 sys.path.append("../")
-import ChatGPT as gpt
-
+import GPT_Key as gpt #type: ignore
 openai.api_key = gpt.key
 
 # openai.api_key = "YOUR_API_KEY"
 
 class ChatGPT:
-    def __init__(self, role) -> None:
+    def __init__(self, role=None) -> None:
         self.Messages =  [
                 {"role": "system", "content": role}       
             ]
@@ -21,17 +20,25 @@ class ChatGPT:
         model="gpt-3.5-turbo",
         messages=self.Messages
         )
-
-        
         self.Messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
-
         return response["choices"][0]["message"]["content"]
     
+    def Loader(self, Log):
+        self.Messages.append(Log)
+    
+    
     def Saving_Messages(self) -> None:
-        title = "Please Provide a 3 Worded Title for this conversation. NOTE: Only 3 Words and should described what we talked about and don't end the words with '.'(Fullstop)"
+        title = "Please Provide a 3 Worded Title for this conversation. NOTE: Only 3 Words and should described what we talked about and don't end the words with '.'(Fullstop) not even inside Double Quotes or Single Quotes. NOTE (Carefully): Write the Title in this Format Your_Conversation_Title OR Conversation_With_You, etc"
         Title = self.ask(title)
-        with open(f"{Title}.log", "x+") as log:
-            for Message in self.Messages:
-                log.write(str(f"{Message}\n"))
+        self.Messages.pop()
+        self.Messages.pop()
+        with open(f"{Title}.log", "a+") as log:
+            log.write(str(self.Messages))
             log.close()
-        os.system("move *.log logs")
+        os.system("move.bat")
+        
+    def Updating_Messages(self, filename):
+        with open(filename, "w") as f:
+            f.write(str(self.Messages))
+            f.close()
+        os.system("move.bat")
--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-import Functions as F
+from Functions import ChatGPT
 from time import sleep
+import os
 
-role = input("Enter a role: ")
-ZarZebGPT = F.ChatGPT(role)
+Logs = os.listdir("./logs")
+for index, log in enumerate(Logs):
+    print(f"{index}. {log}")
+
+choice = input("Please Select a Conversation or enter `new` to start a new Chat: ")
+new_bool = True
+
+if choice == "new" or choice == "": 
+    new_bool = True
+    Role = input("Enter a role: ")
+    ZarZebGPT = ChatGPT(role=Role)
+
+
+elif int(choice) in range(len(Logs)):
+    new_bool = False
+    with open(f"./logs/{Logs[int(choice)]}", "r") as l:
+        log_text = l.read()
+        log_text2 = eval(log_text)
+    ZarZebGPT = ChatGPT()
+    ZarZebGPT.Messages = list(log_text2)
+
+else:
+    print("Invalid choice. Please try again.")
+    exit()
+
 while True:
     try:
         Question = input("""You: """)
-        if Question == "quit":
+        if Question == "q":
             break
         Answer = ZarZebGPT.ask(Question)
         Answer = "ChatGPT: " + Answer
@@ -14,8 +38,13 @@ while True:
             print(char, end="", flush=True)
             sleep(0.01)
         print("\n")
-    except KeyboardInterrupt:
-        print("\n\nEnter 'quit' to exit")
-
+        print("-----------------------------------------------------------------------------------------")
+    except KeyboardInterrupt or ConnectionError:
+        print("\n\nPlease try again Later or user `q` to quit.")
 print("GoodBye!")
-ZarZebGPT.Saving_Messages()
+
+if new_bool == True:
+    ZarZebGPT.Saving_Messages()
+    
+else:
+    ZarZebGPT.Updating_Messages(Logs[int(choice)])
